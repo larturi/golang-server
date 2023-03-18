ee8f6c1049d76d489a748c41877b4cf98901bad9
Leandro Arturi <lea.arturi@gmail.com>
2023-03-18 11:01:07 -0300

    Add ToJson Method

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	server := NewServer(":3100")
 	server.Handle("GET", "/", HandleRoot)
+	server.Handle("POST", "/create", HandlePostRequest)
+	server.Handle("POST", "/user", UserPostRequest)
 	server.Handle("POST", "/api", server.AddMiddleware(
 		HandleHome,
 		CheckAuth(),
